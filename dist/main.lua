db13f77a5840348cb301e818f5844eeb4e9e0db6
CapsAdmin <eliashogstvedt@gmail.com>
2022-05-27 00:46:41 +0200

    assets

--- dist/main.lua
+++ dist/main.lua
@@ -2376,8 +2376,177 @@ love.physics.setMeter(64)
 local world = love.physics.newWorld(0, 0, true)
 
 return world end ]=======], '@src/game/world.nlua'))()
-IMPORTS['src/game/dog.nlua'] = assert(loadstring([=======[ return function()       
+IMPORTS['src/game/vec2.nlua'] = assert(loadstring([=======[ return function() 
+local Vec2 = {}
+Vec2.__index = Vec2
 
+   
+   
+      
+      
+
+
+    
+
+function Vec2:__tostring()
+    return ("Vec2(%f, %f)"):format(self.x, self.y)
+end
+
+local function constructor(_, x, y)
+    return setmetatable({x = x, y = y}, Vec2)
+end
+
+local op = {
+    __add = "+",
+    __sub = "-",
+    __mul = "*",
+    __div = "/",
+}
+
+for key, op in pairs(op) do
+    local code = [[
+        local Vec2 = ...
+        function Vec2.]]..key..[[(a--[=[#: Vec2.@Self]=], b--[=[#: number | Vec2.@Self]=])
+            if type(b) == "number" then
+                return Vec2(a.x ]]..op..[[ b, a.y ]]..op..[[ b)
+            end
+            return Vec2(a.x ]]..op..[[ b.x, a.y ]]..op..[[ b.y)
+        end
+    ]]
+    assert(loadstring(code))(Vec2)
+end
+
+function Vec2.__eq(a, b)
+    return a.x == b.x and a.y == b.y
+end
+
+function Vec2:GetLength()
+    return math.sqrt(self.x * self.x + self.y * self.y)
+end
+
+Vec2.__len = Vec2.GetLength
+
+
+function Vec2.GetDot(a, b)
+	return
+		a.x * b.x +
+		a.y * b.y
+end
+
+
+function Vec2:GetNormalized()
+    local len = self:GetLength()
+    if len == 0 then return Vec2(0, 0) end
+    return self / len
+end
+
+function Vec2:GetRad()
+	return math.atan2(self.x, self.y)
+end
+
+function Vec2:Copy()
+    return Vec2(self.x, self.y)
+end
+
+function Vec2:Floor()
+    return Vec2(math.floor(self.x), math.floor(self.y))
+end
+
+function Vec2.Lerp(a, b, t)
+    return a + (b - a) * t
+end
+
+function Vec2:Unpack()
+    return self.x, self.y
+end
+
+function Vec2:GetRotated(angle)
+    local self = self:Copy()
+	local cs = math.cos(angle);
+	local sn = math.sin(angle);
+
+	local xx = self.x * cs - self.y * sn;
+	local yy = self.x * sn + self.y * cs;
+
+	self.x = xx
+	self.y = yy
+
+	return self
+end
+
+function Vec2:GetReflected(normal)
+	local proj = self:GetNormalized()
+	local dot = proj:GetDot(normal)
+
+  return Vec2(2 * (-dot) * normal.x + proj.x, 2 * (-dot) * normal.y + proj.y) * self:GetLength()
+end
+
+setmetatable(Vec2, {__call = constructor})
+
+return Vec2 end ]=======], '@src/game/vec2.nlua'))()
+IMPORTS['src/game/poop.nlua'] = assert(loadstring([=======[ return function()        
+local Vec2 = IMPORTS['src/game/vec2.nlua']("./vec2.nlua")
+    
+local Poop = {}
+Poop.__index = Poop
+
+   
+
+   
+	  
+	  
+	  
+      
+
+
+    
+
+local settings = {
+	mipmaps = false,
+	linear = false,
+}
+
+local poops = {}
+for i = 1, 3 do
+    poops[i] = love.graphics.newImage("assets/poop/"..i..".png", settings)
+end
+
+function Poop:Update()
+
+end
+
+function Poop:Draw()
+	love.graphics.setColor(1,1,1)
+	local w, h = self.image:getDimensions()
+
+	local scalew = (1 / w) * 40
+	local scaleh = (1 / h) * 40
+
+	love.graphics.draw(self.image, self.body:getX(), self.body:getY(), 0, scalew, scaleh, w/2, h/2)
+end
+
+function Poop.New(pos)
+	local body = love.physics.newBody(_G.physics_world, pos.x, pos.y, "dynamic")
+	local shape = love.physics.newCircleShape(20)
+	local fixture = love.physics.newFixture(body, shape, 1)
+
+    body:setLinearDamping(15)
+
+
+	return setmetatable({
+		body = body,
+		shape = shape,
+		fixture = fixture,
+		image = poops[math.random(1, #poops)],
+	}, Poop)
+end
+
+return Poop
+
+ end ]=======], '@src/game/poop.nlua'))()
+IMPORTS['src/game/dog.nlua'] = assert(loadstring([=======[ return function()        
+local Vec2 = IMPORTS['src/game/vec2.nlua']("./vec2.nlua")
+local Poop = IMPORTS['src/game/poop.nlua']("./poop.nlua")
 local Dog = {}
 Dog.__index = Dog
 
@@ -2387,40 +2556,170 @@ Dog.__index = Dog
 	  
 	  
 	  
+	  
+	  
 
 
     
 
-function Dog:Update(dt)
-
+function Dog:IsPooping()
+	return love.timer.getTime()%7 < 2
 end
 
+function Dog:Update(dt)
+
+	if self:IsPooping() then
+		if not self.pooped then
+			table.insert(_G.objects, Poop.New(Vec2(self.body:getX(), self.body:getY())))
+			self.pooped = true
+		end
+	return end
+
+	self.pooped = false
+
+	local x, y = love.mouse.getPosition()
+	if x == 0 or y == 0 then return end -- wtf
+	local mpos = Vec2(x,y)
+	local pos = Vec2(self.body:getPosition())
+	local dir = mpos - pos
+	local velocity = dir:GetNormalized() * 500
+	if dir:GetLength() > 50 then
+		self.body:applyForce(velocity:Unpack())
+	end
+	self.frame = self.frame + Vec2(self.body:getLinearVelocity()):GetLength() / 1300
+end
+
+local settings = {
+	mipmaps = false,
+	linear = false,
+}
+
+local walk1 = love.graphics.newImage("assets/dog/walk1.png", settings)
+local walk2 = love.graphics.newImage("assets/dog/walk2.png", settings)
+local poop = love.graphics.newImage("assets/dog/poop.png", settings)
+
+function Dog:GetWalkImage()
+	local frame = math.ceil(self.frame)
+	return frame%2 == 0 and walk1 or walk2
+end
+
+
 function Dog:Draw()
-	love.graphics.setColor(0.76, 1, 0.05)
-	love.graphics.circle("fill", self.body:getX(), self.body:getY(), self.shape:getRadius())
+	love.graphics.setColor(1,1,1)
+
+	local dir = Vec2(self.body:getLinearVelocity())
+	local img = self:GetWalkImage()
+
+	if self:IsPooping() then
+		img = poop
+	end
+
+	local w, h = img:getDimensions()
+
+	local scalew = (1 / w) * 100
+	local scaleh = (1 / h) * 100
+
+	love.graphics.draw(img, self.body:getX(), self.body:getY(), 0, dir.x > 0 and scalew or -scalew, scaleh, w/2, h/2)
 end
 
 function Dog.New()
 	local body = love.physics.newBody(_G.physics_world, 650/2, 650/2, "dynamic")
 	local shape = love.physics.newCircleShape( 20)
 	local fixture = love.physics.newFixture(body, shape, 1)
+
+	body:setLinearDamping(5)
 	fixture:setRestitution(0.9)
 
 	return setmetatable({
 		body = body,
 		shape = shape,
 		fixture = fixture,
+		frame = 0,
+		pooped = false,
 	}, Dog)
 end
 
 return Dog
 
  end ]=======], '@src/game/dog.nlua'))()
+IMPORTS['src/game/trash.nlua'] = assert(loadstring([=======[ return function()        
+local Vec2 = IMPORTS['src/game/vec2.nlua']("./vec2.nlua")
+    
+local Trash = {}
+Trash.__index = Trash
+
+   
+
+   
+	  
+	  
+	  
+      
+      
+
+
+    
+
+local settings = {
+	mipmaps = false,
+	linear = false,
+}
+
+local poops = {}
+for i = 1, 5 do
+    poops[i] = love.graphics.newImage("assets/trash/"..i..".png", settings)
+end
+
+function Trash:Update()
+
+end
+
+function Trash:Draw()
+	love.graphics.setColor(1,1,1)
+	local w, h = self.image:getDimensions()
+
+	local scalew = (1 / w) * 60
+	local scaleh = (1 / h) * 60
+
+	love.graphics.draw(self.image, self.body:getX(), self.body:getY(), self.rotation, scalew, scaleh, w/2, h/2)
+end
+
+function Trash.New(pos)
+	local body = love.physics.newBody(_G.physics_world, pos.x, pos.y, "dynamic")
+	local shape = love.physics.newCircleShape(20)
+	local fixture = love.physics.newFixture(body, shape, 1)
+
+    body:setLinearDamping(15)
+
+
+	return setmetatable({
+		body = body,
+		shape = shape,
+		fixture = fixture,
+		image = poops[math.random(1, #poops)],
+        rotation = math.random() * math.pi * 2,
+	}, Trash)
+end
+
+return Trash
+
+ end ]=======], '@src/game/trash.nlua'))()
 IMPORTS['src/game/init.nlua'] = assert(loadstring([=======[ return function() _G.physics_world = IMPORTS['src/game/world.nlua']("./world.nlua")
-
+_G.objects = {}
 local Dog = IMPORTS['src/game/dog.nlua']("./dog.nlua")
+local Trash = IMPORTS['src/game/trash.nlua']("./trash.nlua")
+local Vec2 = IMPORTS['src/game/vec2.nlua']("./vec2.nlua")
 
-local dog = Dog.New()
+table.insert(objects, Dog.New())
+
+for i = 1, 10 do
+    table.insert(objects, Trash.New(
+        Vec2(
+            math.random(1, love.graphics.getWidth()), 
+            math.random(1, love.graphics.getHeight())
+        )
+    ))
+end
 
 love.graphics.setBackgroundColor(1, 1, 1)
 
@@ -2428,7 +2727,9 @@ event.AddListener({
     event_type = "draw",
     id = "draw",
     callback = function() 
-        dog:Draw()
+        for i,v in ipairs(_G.objects) do
+            v:Draw()
+        end
     end,
 })
 
@@ -2437,11 +2738,13 @@ event.AddListener({
     id = "update",
     callback = function(dt) 
         _G.physics_world:update(dt)
-        dog:Update(dt)
+        for i,v in ipairs(_G.objects) do
+            v:Update(dt)
+        end
     end,
 })
 
-_G.refs = {dog} end ]=======], '@src/game/init.nlua'))()
+ end ]=======], '@src/game/init.nlua'))()
 IMPORTS['src/main.nlua'] = assert(loadstring([=======[ return function()               
    
 
